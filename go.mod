e46425d481cfbf17a0ae067f1948e847da17539b
KrishVij <official.krishvij@gmail.com>
2025-06-15 23:35:53 +0530

    Modularized My Code

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/KrishVij/clip2ASCII
 
 go 1.24.3
 
-require golang.org/x/image v0.28.0
+require (
+	github.com/KononK/resize v0.0.0-20200801203131-21c514740ed6
+	golang.org/x/image v0.28.0
+)
 
 require golang.org/x/text v0.26.0 // indirect
--- main.go
+++ main.go
@@ -1,21 +1,33 @@
 package main
 
 import (
+	"log"
 
 	"github.com/KrishVij/clip2ASCII/Frame_Processing"
 )
 
 func main() {
 
-	Image := Frame_Processing.LoadImage()
+	Image, err := Frame_Processing.ImageResizeAndLoad()
 
-	if Image != nil {
+	if err != nil {
 
-		defer Image.Close()
+		log.Fatalf("ERROR OCCURED WHILE LOADING IMAGE: %v", err)
 	}
 
-	Pixels := Frame_Processing.GetBrightnessMatrix(Image)
-	asciiImage := Frame_Processing.GrayScaleToAscii(Pixels)
+	Pixels,rgbaValues := Frame_Processing.ProcessImageForAscii(Image)
+	asciiImage,err := Frame_Processing.GrayScaleToAscii(Pixels,rgbaValues)
+	if err != nil {
+
+		log.Fatalf("ERROR OCCURED WHILE CONVERTING TO ASCII: %v",err)
+		
+	}
 	Frame_Processing.SaveImage(asciiImage)
+	if err != nil {
+
+		log.Fatalf("Error occured while saving the image: %v",err)
+	}
+
+	log.Println("ASCII image generated successfully!")
 
 }
