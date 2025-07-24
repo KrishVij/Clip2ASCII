5d378e213dc570c45609143ad6ab62476574188f
KrishVij <official.krishvij@gmail.com>
2025-07-24 21:19:32 +0530

    Added StichFramesToVideo Functionality

--- FFmpeg_Utils/FFmpeg_Operations.go
+++ FFmpeg_Utils/FFmpeg_Operations.go
@@ -12,21 +12,21 @@ func ExtarctFramesFromVideo(videoPath string) (framesPath string) {
 
 	err := os.Mkdir("C:/Users/Krish Vij/Frames", 0750)
 
-	framesPath = filepath.Join("C:/Users/Krish Vij","Frames")
+	framesPath = filepath.Join("C:/Users/Krish Vij", "Frames")
 
 	if err != nil {
 
-		log.Fatalf("Error Occured while Creating the File: %v",err)
+		log.Fatalf("Error Occured while Creating the File: %v", err)
 	}
-	
+
 	outputPattern := filepath.Join("C:/Users/Krish Vij/Frames", "%03d.png")
-	
+
 	cmd := exec.Command("ffmpeg", "-i", videoPath, outputPattern)
-	output,err := cmd.Output()
+	output, err := cmd.Output()
 
 	if err != nil {
 
-		log.Fatalf("Error Occured while opening the ffmpeg Path: %v",err)
+		log.Fatalf("Error Occured while opening the ffmpeg Path: %v", err)
 	}
 
 	fmt.Println(string(output))
@@ -35,11 +35,21 @@ func ExtarctFramesFromVideo(videoPath string) (framesPath string) {
 
 }
 
-// func StitchFramesToVideo(pathToFrames string) {
+func StitchFramesToVideo(outputPATH string) {
 
-// 	inputFile,err := os.Open("C:/Users/Krish Vij/ASCII_Video")
-// 	if err != nil {
+	err := os.Chdir("C:/Users/Krish Vij/ASCII_Frames")
+	
+	if err != nil {
+		
+		log.Fatalf("Error changing directory: %v", err)
+	}
 
-// 		log.Fatalf("Error Occured while Creating the file: %v",err)
-// 	}
-// }
+	cmd := exec.Command("ffmpeg", "-framerate", "30", "-i", "ASCII_Frames%03d.png", outputPATH)
+	output, err := cmd.CombinedOutput()
+
+	if err != nil {
+
+		log.Fatalf("FFmpeg error: %v\nOutput: %s", err, string(output))
+	}
+
+}
