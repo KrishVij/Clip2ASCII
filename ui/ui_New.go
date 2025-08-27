fd3dcddf4bc9fef2a23de0a004a59fd38168e6c8
KrishVij <official.krishvij@gmail.com>
2025-08-27 13:44:23 +0530

    Made this the main file for running the App

--- ui/ui_New.go
+++ ui/ui_New.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	// "os/exec"
 	"time"
+	"sync"
+	"path/filepath"
 
 	stdImage "image"
 	"image/color"
@@ -14,23 +16,20 @@ import (
 	"github.com/KononK/resize"
 	"github.com/hajimehoshi/ebiten/v2"
 
-	// "github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	FFmpegutils "github.com/KrishVij/clip2ASCII/FFmpeg_Utils"
 	"github.com/ebitenui/ebitenui"
 	"github.com/ebitenui/ebitenui/image"
 	"github.com/ebitenui/ebitenui/widget"
 
-	// "github.com/hajimehoshi/ebiten/v2"
-	// "github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/text/v2"
 	"github.com/ncruces/zenity"
 	"golang.org/x/image/font/gofont/goregular"
+	"github.com/KrishVij/clip2ASCII/Frame_Processing"
 )
 
-var my_color = color.NRGBA{R: 25, G: 23, B: 36, A: 255}
+var my_color = color.NRGBA{R: 242, G: 233, B: 225, A: 255}
 var rosePinePine color.Color = my_color
-
-const defaultPath = "C:/Users/Krish Vij/Downloads"
+var selected_video_path string
 
 // var newPage = image.NewImageColor(rosePinePurple)
 
@@ -43,15 +42,16 @@ type Game struct {
 	resized_thumbnail_ebiten_image_format *ebiten.Image
 	btn_CHTOFD                            *widget.Button
 	screen_height, screen_width           int
+	thumbnail_widget                      *widget.Graphic
 }
 
 func loadImageInvisible() (*widget.ButtonImage, error) {
 
-	idle := image.NewBorderedNineSliceColor(color.NRGBA{R: 144, G: 122, B: 169, A: 255}, color.NRGBA{144, 122, 169, 255}, 3)
+	idle := image.NewBorderedNineSliceColor(color.NRGBA{R: 42, G: 39, B: 63, A: 255}, color.NRGBA{42, 39, 63, 255}, 3)
 
-	hover := image.NewBorderedNineSliceColor(color.NRGBA{R: 144, G: 122, B: 169, A: 255}, color.NRGBA{144, 122, 169, 255}, 3)
+	hover := image.NewBorderedNineSliceColor(color.NRGBA{R: 42, G: 39, B: 63, A: 255}, color.NRGBA{42, 39, 63, 255}, 3)
 
-	pressed := image.NewAdvancedNineSliceColor(color.NRGBA{R: 144, G: 122, B: 169, A: 255}, image.NewBorder(3, 2, 2, 2, color.NRGBA{246, 193, 119, 255}))
+	pressed := image.NewAdvancedNineSliceColor(color.NRGBA{R: 42, G: 39, B: 63, A: 255}, image.NewBorder(3, 2, 2, 2, color.NRGBA{42, 39, 63, 255}))
 
 	return &widget.ButtonImage{
 		Idle:    idle,
@@ -62,11 +62,11 @@ func loadImageInvisible() (*widget.ButtonImage, error) {
 
 func loadImage() (*widget.ButtonImage, error) {
 
-	idle := image.NewBorderedNineSliceColor(color.NRGBA{R: 234, G: 157, B: 52, A: 255}, color.NRGBA{234, 157, 52, 255}, 3)
+	idle := image.NewBorderedNineSliceColor(color.NRGBA{R: 215, G: 130, B: 126, A: 255}, color.NRGBA{234, 157, 52, 255}, 3)
 
-	hover := image.NewBorderedNineSliceColor(color.NRGBA{R: 234, G: 157, B: 52, A: 255}, color.NRGBA{234, 157, 52, 255}, 3)
+	hover := image.NewBorderedNineSliceColor(color.NRGBA{R: 215, G: 130, B: 126, A: 255}, color.NRGBA{234, 157, 52, 255}, 3)
 
-	pressed := image.NewAdvancedNineSliceColor(color.NRGBA{R: 246, G: 193, B: 119, A: 255}, image.NewBorder(3, 2, 2, 2, color.NRGBA{246, 193, 119, 255}))
+	pressed := image.NewAdvancedNineSliceColor(color.NRGBA{R: 215, G: 130, B: 126, A: 255}, image.NewBorder(5, 5, 5, 5, color.NRGBA{246, 193, 119, 255}))
 
 	return &widget.ButtonImage{
 		Idle:    idle,
@@ -91,6 +91,12 @@ func loadFont(size float64) (text.Face, error) {
 
 func main() {
 
+	user_home_directory, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatalf("Couldnt Find Your Home Directory: %v", err)
+	}
+
+	defaultPath := user_home_directory
 	game := &Game{}
 
 	Min, Current, Max := 0, 0, 10
@@ -99,7 +105,7 @@ func main() {
 
 		widget.ContainerOpts.BackgroundImage(image.NewNineSliceColor(color.NRGBA{
 
-			0x90, 0x7a, 0xa9, 0xff,
+			0x20, 0x27, 0x3f, 0xff,
 		})),
 
 		widget.ContainerOpts.Layout(widget.NewAnchorLayout()),
@@ -135,14 +141,14 @@ func main() {
 			}),
 		),
 	)
-
+	
 	btn_Invisible_Two := widget.NewButton(
 
 		widget.ButtonOpts.Image(ButtonImageInvisible),
 
-		widget.ButtonOpts.Text("ClickHereToOpenFileDialog", Face, &widget.ButtonTextColor{
+		widget.ButtonOpts.Text("", Face, &widget.ButtonTextColor{
 
-			Idle: color.NRGBA{144, 122, 169, 255},
+			Idle: color.NRGBA{42, 63, 39, 255},
 		}),
 
 		widget.ButtonOpts.TextProcessBBCode(false),
@@ -169,7 +175,7 @@ func main() {
 
 		widget.ButtonOpts.Image(ButtonImage),
 
-		widget.ButtonOpts.Text("ClickHereToOpenFileDialog", Face, &widget.ButtonTextColor{
+		widget.ButtonOpts.Text("Select Video", Face, &widget.ButtonTextColor{
 
 			Idle: color.NRGBA{0, 0, 0, 255},
 		}),
@@ -195,13 +201,21 @@ func main() {
 
 		widget.ButtonOpts.ClickedHandler(func(args *widget.ButtonClickedEventArgs) {
 
-			filepath, err := zenity.SelectFile(
+			var err error
+			selected_video_path, err = zenity.SelectFile(
 
 				zenity.Filename(defaultPath),
 				zenity.FileFilters{{
 
 					Name:     "Video files",
-					Patterns: []string{"*.mp4"},
+					Patterns: []string{
+						"*.mp4",
+						"*.avi",
+						"*.mkv",
+						"*.mpeg",
+						"*.mpg",
+						"*.mov",
+					},
 					CaseFold: true,
 				}},
 			)
@@ -210,8 +224,44 @@ func main() {
 
 				log.Fatalf("Error Ocuured while Opening file dialogs : %v", err)
 			}
+			
+			if FFmpegutils.Check_Duration(selected_video_path) == true {
 
-			thumbnail_file_path := FFmpegutils.Extract_Thumbnail(filepath)
+				fmt.Print("That Your Video is correct Duration!!")
+			}
+			
+			if FFmpegutils.Check_Duration(selected_video_path) == false {
+
+				textContainer_for_btn_CHTOFD := widget.NewContainer(
+					// The container will use a vertical row layout to lay out the progress
+					// bars in a vertical row.
+					widget.ContainerOpts.Layout(widget.NewRowLayout(
+						widget.RowLayoutOpts.Direction(widget.DirectionVertical),
+						widget.RowLayoutOpts.Spacing(180),
+					)),
+					// Set the required anchor layout data to determine where in the root
+					// container to place the progress bars.
+					widget.ContainerOpts.WidgetOpts(
+						widget.WidgetOpts.LayoutData(widget.AnchorLayoutData{
+							HorizontalPosition: widget.AnchorLayoutPositionCenter,
+							VerticalPosition:   widget.AnchorLayoutPositionStart,
+						}),
+					),
+				)
+
+				text_for_video_too_long := widget.NewText(
+
+					widget.TextOpts.Text("Your Video is Too Long!!", Face, rosePinePine),
+					widget.TextOpts.Position(widget.TextPositionStart, widget.TextPositionStart),
+				)
+
+				textContainer_for_btn_CHTOFD.AddChild(text_for_video_too_long)
+				rootContainer.RemoveChildren()
+				rootContainer.AddChild(textContainer_for_btn_CHTOFD)
+				
+			}
+			
+			thumbnail_file_path := FFmpegutils.Extract_Thumbnail(selected_video_path)
 
 			f, err := os.Open(thumbnail_file_path)
 			if err != nil {
@@ -348,8 +398,10 @@ func main() {
 			textContainer.AddChild(text)
 			rootContainer.AddChild(textContainer)
 			progressBarsContainer.AddChild(progressBar)
+			game.resized_thumbnail_ebiten_image_format.Deallocate()
 			rootContainer.AddChild(progressBarsContainer)
-
+			
+			
 			go func() {
 
 				for Current < Max {
@@ -358,7 +410,116 @@ func main() {
 					progressBar.SetCurrent(Current)
 					time.Sleep(50 * time.Millisecond)
 				}
+
+				text_to_notify_video_is_converted := widget.NewText(
+
+					widget.TextOpts.Text("Your ASCII Video Is Ready Check Your File Explorer!!", Face, rosePinePine),
+					widget.TextOpts.Position(widget.TextPositionStart, widget.TextPositionStart),
+				)
+
+				rootContainer.RemoveChildren()
+				textContainer.RemoveChild(text)
+				textContainer.AddChild(text_to_notify_video_is_converted)
+				rootContainer.AddChild(textContainer)
+
+				videoPath := selected_video_path
+				// if FFmpegutils.Check_Duration(videoPath) == false {
+
+				// 	text_container_for_btn_TOASCII := widget.NewContainer(
+
+				// 		widget.WidgetOpts.ContainerOpts(widget.AnchorLayout{
+
+				// 			widget.ContainerOpts.AnchorLayoutData{
+
+				// 				HorizontalPosition:
+				// 			}
+				// 		})
+				// 	)
+				// 	text_to_notify_video_is_too_long := widget.NewText(
+
+				// 		widget.TextOpts.Text("Your Video Is Too Long!!", Face, rosePinePine),
+				// 		widget.TextOpts.Position(widget.TextPositionStart, widget.TextPositionStart),
+				// 	)
+				
+				// }
+				path_to_output_ascii_video_file := filepath.Join(user_home_directory, "output.mp4")
+				outputPATH := path_to_output_ascii_video_file
+				
+				// mpp := Frame_Processing.Calculate_Ink_Required_For_Drawing_ASCII_Chars()
+				// Frame_Processing.Generate_ASCII_Lookup_Table(mpp)
+
+				// path_to_ASCII_Frames := filepath.Join(user_home_directory, "ASCII_Frames")
+				// if err := os.MkdirAll(path_to_ASCII_Frames, 0750); err != nil {
+
+				// 	log.Println("Error Occured While Creating ASCII Frames Directory: %v", err)
+				// }
+
+				// Count := 1
+				
+				result := FFmpegutils.ExtractFramesFromVideo(videoPath)
+
+				directory, err := os.ReadDir(result)
+				if err != nil {
+
+					log.Fatalf("Error Occured While Reading Contents of The Frames Folder: %v", err)
+				}
+
+				var wg sync.WaitGroup
+
+				for i, item := range directory {
+
+					wg.Add(1)
+
+					go func(count int, filename string) {
+
+						defer wg.Done()
+
+						framePath := filepath.Join(result, filename)
+
+						Image, err := Frame_Processing.LoadAndResizeImage(framePath)
+
+						if err != nil {
+
+							log.Fatalf("ERROR OCCURED WHILE LOADING IMAGE: %v", err)
+						}
+
+						Pixels, rgbaValues := Frame_Processing.ExtractPixelData(Image)
+						asciiImage, err := Frame_Processing.RenderAsciiImage(Pixels, rgbaValues)
+						if err != nil {
+
+							log.Fatalf("ERROR OCCURED WHILE CONVERTING TO ASCII: %v", err)
+
+						}
+						err = Frame_Processing.SaveImage(asciiImage, count)
+						if err != nil {
+
+							log.Fatalf("Error occured while saving the image: %v", err)
+						}
+						
+						fmt.Printf("Frame: %d processed successfully\n", count-1)
+					}(i + 1, item.Name())
+
+				}
+
+				wg.Wait()
+
+				log.Println("ASCII frames folder generated successfully!")
+
+				FFmpegutils.StitchFramesToVideo(outputPATH)
+
+				fmt.Println("ASCII Video Generated successfully")
 			}()
+
+			
+			// text_to_notify_video_is_converted := widget.NewText(
+
+			// 	widget.TextOpts.Text("Your ASCII Video is ready check your File Explorer", Face, rosePinePine),
+			// 	widget.TextOpts.Position(widget.TextPositionStart, widget.TextPositionStart),
+			// )
+
+			// textContainer.AddChild(text_to_notify_video_is_converted)
+			// rootContainer.AddChild(textContainer)
+			
 
 			println("Buttons is Clicked")
 
@@ -382,6 +543,7 @@ func main() {
 	// buttonGroup2.AddChild(btn_FromASCII)
 	// rootContainer.AddChild(buttonGroup1)
 	rootContainer.AddChild(buttonGroup2)
+	//	rootContainer.AddChild(thumbnail_container)
 
 	ui := ebitenui.UI{
 
@@ -400,7 +562,7 @@ func main() {
 	game.btn_CHTOFD = game.btn_CHTOFD
 	// game.ButtonTraversal(ButtonImageInvisible)
 
-	err := ebiten.RunGame(game)
+	err = ebiten.RunGame(game)
 	if err != nil {
 		log.Println(err)
 	}
@@ -423,6 +585,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		op.GeoM.Translate(float64((g.screen_width - img_width)/2), float64((g.screen_height - img_height)/2))
 		screen.DrawImage(g.resized_thumbnail_ebiten_image_format, op)
 	}
+	
 }
 
 func (g *Game) Layout(w, h int) (int, int) {
